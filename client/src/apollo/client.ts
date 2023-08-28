3cad166200b7bf7667dc09f32e63e00a8a7176e7
WhoStoleMySleep <haymenko.n+naumenko@gmail.com>
2023-08-28 07:14:06 +0700

    feat: use server

--- client/src/apollo/client.ts
+++ client/src/apollo/client.ts
@@ -1,8 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  // uri: 'https://sreverokdlyasaitaddsd.onrender.com/graphql',
-  uri: 'http://192.168.199.115:3005/graphql',
+  uri: 'https://sreverokdlyasaitaddsd.onrender.com/graphql',
   cache: new InMemoryCache(),
 }); 
 
