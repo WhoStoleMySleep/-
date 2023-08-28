1bb34cba21990cbf256da44be610790727e3f8e5
WhoStoleMySleep <haymenko.n+naumenko@gmail.com>
2023-08-28 07:11:22 +0700

    feat: use Redux and Apollo

--- client/src/components/ConfiguratorMenu/ConfiguratorMenu.tsx
+++ client/src/components/ConfiguratorMenu/ConfiguratorMenu.tsx
@@ -2,9 +2,29 @@ import { useState } from 'react';
 import styles from './ConfiguratorMenu.module.scss';
 import { Link } from 'react-router-dom';
 import scrollingBan from '../../utils/scrollingBan';
+import { useMutation } from '@apollo/client';
+import { SAVE_CUSTOM_CAKES } from '../../GraphQl.queries';
 
 function ConfiguratorMenu({configuratorMenuData}: {configuratorMenuData: {setOpen: any}}) {
+  const [saveCustomCakes] = useMutation(SAVE_CUSTOM_CAKES)
   const [step, setStep] = useState(1)
+  const [data, setData] = useState({
+    id: `${Math.floor(Math.random() * (100000000 - 0 + 1)) + 0}`,
+    image: '1',
+    description: '',
+    name: '',
+    email: '',
+    number: '',
+  })
+
+  type ConfiguratorInputData = {
+    id: string
+    image: string
+    description: string
+    name: string
+    email: string
+    number: string
+  }
 
   const onClose = () => {
     configuratorMenuData.setOpen(false)
@@ -22,6 +42,23 @@ function ConfiguratorMenu({configuratorMenuData}: {configuratorMenuData: {setOpe
   }
 
   scrollingBan(true)
+
+  const editData = (param: string, value: string) => {
+    setData((item: ConfiguratorInputData) => ({...item, [param]: value}))
+  }
+
+  function submitOrder() {
+    if (data) {
+      saveCustomCakes({variables: {
+        id: data.id,
+        image: data.image,
+        description: data.description,
+        name: data.name,
+        email: data.email,
+        number: data.number
+      }})
+    }
+  }
   
   return (
     <article className={styles["configurator-menu"]}>
@@ -36,16 +73,16 @@ function ConfiguratorMenu({configuratorMenuData}: {configuratorMenuData: {setOpe
         {step === 2 &&
           <div className={styles["configurator-menu__content-container"]}>
             <p className={styles["configurator-menu__text"]}>Введите описание торта</p>
-            <textarea name="" id="" className={styles["configurator-menu__textarea"]}></textarea>
+            <textarea name="" id="" className={styles["configurator-menu__textarea"]} onChange={(e) => editData('description', e.target.value)}></textarea>
           </div>
         }
         {step === 3 &&
           <div className={styles["configurator-menu__content-container"]}>
             <p className={styles["configurator-menu__text"]}>Контактные данные</p>
             <div className={styles["configurator-menu__input-list"]}>
-              <input type="text" className={styles["configurator-menu__input"]} placeholder='Ваше имя и фамилия' />
-              <input type="text" className={styles["configurator-menu__input"]} placeholder='Ваш e-mail' />
-              <input type="text" className={styles["configurator-menu__input"]} placeholder='+7' />
+              <input type="text" className={styles["configurator-menu__input"]} placeholder='Ваше имя и фамилия' onChange={(e) => editData('name', e.target.value)} />
+              <input type="text" className={styles["configurator-menu__input"]} placeholder='Ваш e-mail' onChange={(e) => editData('email', e.target.value)} />
+              <input type="text" className={styles["configurator-menu__input"]} placeholder='+7' onChange={(e) => editData('number', e.target.value)} />
             </div>
             <p className={styles["configurator-menu__text"]}>
               Менеджер свяжется с вами по телефону для выяснения деталей заказа. <br />
@@ -59,7 +96,7 @@ function ConfiguratorMenu({configuratorMenuData}: {configuratorMenuData: {setOpe
         <div className={styles["configurator-menu__controller"]}>
           <button className={`${styles["configurator-menu__prev-step"]} ${step === 1 ? styles["configurator-menu__prev-step_deactive"] : ''}`} onClick={prevStep}>← Предыдущий шаг</button>
           <div className={styles["configurator-menu__controller-step"]}>{step}/3</div>
-          <button className={`${styles["configurator-menu__next-step"]} ${step === 3 ? styles["configurator-menu__next-step_deactive"] : ''}`} onClick={nextStep}>Следующий шаг →</button>
+          <button className={styles["configurator-menu__next-step"]} onClick={step === 3 ? submitOrder : nextStep}>{step !== 3 ? 'Следующий шаг →' : 'Подтвердить'}</button>
         </div>
       </div>
     </article>
--- client/src/components/SendContact/SendContact.tsx
+++ client/src/components/SendContact/SendContact.tsx
@@ -1,14 +1,39 @@
+import { useEffect, useState } from 'react';
 import styles from './SendContact.module.scss';
+import { gql, useMutation, useQuery } from '@apollo/client';
 
-function SendContact() {
+const SAVE_NUMBER = gql`
+mutation Mutation($createCakesNumberId: String!, $number: String!) {
+  createCakesNumber(id: $createCakesNumberId, number: $number) {
+    id
+    number
+  }
+}
+`
+
+function SendContact() {  
+  const [number, setNumber] = useState('');
+  const [saveNumber] = useMutation(SAVE_NUMBER)
+  
+  const handleSaveNumber = () => {
+    if (number) {
+      saveNumber({
+        variables: {
+          createCakesNumberId: `${Math.floor(Math.random() * (100000000 - 0 + 1)) + 0}`,
+          number: number
+        }
+      })
+    }
+  }
+  
   return (
     <article className={styles["send-contact"]}>
       <h2 className={styles["send-contact__title"]}>
         Оставьте номер и мы поможем с выбором торта
       </h2>
       <span className={styles["send-contact__input-container"]}>
-        <input type="text" className={styles["send-contact__input"]} placeholder='Телефон' />
-        <button className={styles["send-contact__button"]} type='button'>Позвоните мне</button>
+        <input type="text" className={styles["send-contact__input"]} onChange={(e) => setNumber(e.target.value)} placeholder='Телефон' />
+        <button className={`${styles["send-contact__button"]} ${number.length !== 11 && styles["send-contact__button_deactive"]}`} onClick={number.length === 11 ? handleSaveNumber : () => ''} type='button'>Позвоните мне</button>
       </span>
     </article>
   )
